e5d1594d1e12f478a2a193c263c8af6d88889f5f
odrerir <victorcaua03@gmail.com>
2024-02-08 21:52:28 -0300

    50% pronto

--- main.cpp
+++ main.cpp
@@ -3,62 +3,173 @@
 using namespace std;
 
 typedef struct {
+    int id;
     string nome;
     string autor;
     string ano;
     string genero;
     string album;
-
 } Musicas;
 
-void insere(Musicas *musica) {
-    cout << "Nome da musica: ";
-    cin >> musica->nome;
-    cout << endl;
-    cout << "Nome do autor: ";
-    cin >> musica->autor;
-    cout << endl;
-    cout << "Ano da musica: ";
-    cin >> musica->ano;
-    cout << endl;
-    cout << "Qual genero: ";
-    cin >> musica->genero;
-    cout << endl;
-    cout << "Nome do album: ";
-    cin >> musica->album;
-    cout << endl;
+// Variável global para controlar o ID das músicas
+int proximoId = 1;
+
+// Função para mesclar dois subvetores
+void intercala(Musicas *v, int inicio, int meio, int fim) {
+    Musicas *w;
+    w = new Musicas[fim - inicio];
+    int i = inicio, j = meio, k = 0;
+    while (i < meio && j < fim) {
+        if (v[i].nome < v[j].nome) {
+            w[k] = v[i];
+            i++;
+        } else {
+            w[k] = v[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < meio) {
+        w[k] = v[i];
+        i++;
+        k++;
+    }
+    while (j < fim) {
+        w[k] = v[j];
+        j++;
+        k++;
+    }
+    for (k = 0; k < fim - inicio; k++) {
+        v[inicio + k] = w[k];
+    }
+    delete[] w;
 }
 
-void imprime(Musicas *musica) {
-    cout << "Nome da musica: " << musica->nome << endl;
-    cout << "Nome do autor: " << musica->autor << endl;
-    cout << "Ano na musica: " << musica->ano << endl;
-    cout << "Genero da musica: " << musica->genero << endl;
-    cout << "Nome do album: " << musica->album << endl;
+// Função para realizar o Merge Sort
+void mergeSort(Musicas *v, int inicio, int fim) {
+    if (inicio < (fim - 1)) {
+        int meio = (inicio + fim) / 2;
+        mergeSort(v, inicio, meio);
+        mergeSort(v, meio, fim);
+        intercala(v, inicio, meio, fim);
+    }
+}
+
+// Função para imprimir as músicas
+void imprime(Musicas *musicas, int size) {
+    for (int i = 0; i < size; ++i) {
+        cout << "Musica numero " << i + 1 << endl;
+        cout << "Nome da musica: " << musicas[i].nome << endl;
+        cout << "Nome do autor: " << musicas[i].autor << endl;
+        cout << "Ano na musica: " << musicas[i].ano << endl;
+        cout << "Genero da musica: " << musicas[i].genero << endl;
+        cout << "Nome do album: " << musicas[i].album << endl;
+        cout << endl;
+        cout << endl;
+    }
+}
+
+// Função para busca binária em arr[] pelo ID da música
+int buscaPorId(const Musicas *v, int id, int inicio, int fim) {
+    while (inicio <= fim) {
+        int m = inicio + (fim - inicio) / 2;
+        if (v[m].id == id)
+            return m;
+        if (v[m].id < id)
+            inicio = m + 1;
+        else
+            fim = m - 1;
+    }
+    return -1;
+}
+
+// Função para inserir uma nova música
+void insereMusica(Musicas *&musicas, int &size) {
+    Musicas novaMusica;
+    novaMusica.id = proximoId++; // Atribuindo o próximo ID e incrementando o contador
+    cout << "Nome da musica: ";
+    cin >> novaMusica.nome;
+    cout << "Nome do autor: ";
+    cin >> novaMusica.autor;
+    cout << "Ano da musica: ";
+    cin >> novaMusica.ano;
+    cout << "Qual genero: ";
+    cin >> novaMusica.genero;
+    cout << "Nome do album: ";
+    cin >> novaMusica.album;
+
+    // Alocando novo espaço para a música
+    Musicas *temp = new Musicas[size + 1];
+    // Copiando músicas existentes para o novo espaço
+    for (int i = 0; i < size; ++i) {
+        temp[i] = musicas[i];
+    }
+    // Adicionando a nova música
+    temp[size] = novaMusica;
+    ++size;
+    // Liberando o espaço antigo e apontando para o novo espaço
+    delete[] musicas;
+    musicas = temp;
+    // Ordenando as músicas após a inserção da nova música
+    mergeSort(musicas, 0, size - 1);
+
+    // Atribuindo IDs conforme a sequência ordenada
+    for (int i = 0; i < size; ++i) {
+        musicas[i].id = proximoId++;
+    }
 }
 
 int main() {
-    int p;
-    Musicas *musica = new Musicas(); // Alocação dinâmica
+    Musicas *musicas = nullptr;
+    int size = 0;
+    int opcao;
+
     do {
         cout << "Bem vindo ao banco de musicas!!!" << endl;
         cout << "Digite o numero referente a tarefa que deseja." << endl;
         cout << "1- Imprimir todas as musicas" << endl;
-        cout << "2- inserir nova musica" << endl;
+        cout << "2- Inserir nova musica" << endl;
+        cout << "3- Buscar musica pelo ID" << endl;
+        cout << "5- Sair" << endl;
 
-        switch (p) {
+        cin >> opcao;
+
+        switch (opcao) {
             case 1:
-                imprime(musica); 
+                if (size > 0) {
+                    cout << "Listando todas as musicas:" << endl;
+                    imprime(musicas, size);
+                } else {
+                    cout << "Nenhuma musica para imprimir." << endl;
+                    cout << endl;
+                }
                 break;
             case 2:
-                insere(musica); 
+                insereMusica(musicas, size);
+                break;
+            case 3:
+                if (size > 0) {
+                    int idMusica;
+                    cout << "Digite o ID da musica que deseja buscar: ";
+                    cin >> idMusica;
+                    int resultado = buscaPorId(musicas, idMusica, 0, size - 1);
+                    if (resultado != -1) {
+                        cout << "Musica encontrada na posicao " << resultado << ":" << endl;
+                        imprime(&musicas[resultado], 1);
+                    } else {
+                        cout << "Musica nao encontrada." << endl;
+                    }
+                } else {
+                    cout << "Nenhuma musica para buscar." << endl;
+                }
+                break;
+            case 5:
+                cout << "Encerrando o programa." << endl;
                 break;
             default:
-                break;
+                cout << "Opcao invalida." << endl;
         }
-    } while (p != 6);
-
-    delete musica; 
-
-    return 0;
+    } while (opcao != 5);
+   
 }
